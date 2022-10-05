a0ef129f0d5ac1d2169553209b8cfca6b60d2fa8
EmanRiziq <emanriziq@gmail.com>
2022-10-05 23:24:39 +0300

    lab-14

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
-// import Post from './components/Post';
-// import Signin from './components/Signin';
-// import Signup from './components/Signup';
 import Auth from './components/Auth';
 import React, { Component } from 'react';
-// import { When } from 'react-if';
-// import { useEffect, useContext } from 'react';
 import AuthContextProvider from './Context/AuthContext';
-
+import PostContextProvider from './Context/PostContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <AuthContextProvider>
+      <PostContextProvider>
       <div>
         <Auth />
       </div>
+      </PostContextProvider>
     </AuthContextProvider>
   );
 }
